1b203dcb180929d7f7a40cf4d590acdcb201f84d
tman57 <70616151+dez03@users.noreply.github.com>
2023-09-18 19:33:21 -0400

    final push to PC

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
-export default function App() {
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [age, setAge] = useState("");
+
+  const handleLogin = () => {
+   
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Email:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+        autoCapitalize="none" // Disable capitalization
+        keyboardType="email-address" // Set keyboardType to 'email-address'
+      />
+
+      <Text style={styles.label}>Password:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
+
+      <Text style={styles.label}>First Name:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="First Name"
+        onChangeText={(text) => setFirstName(text)}
+        value={firstName}
+      />
+
+      <Text style={styles.label}>Age:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Age"
+        keyboardType="numeric"
+        onChangeText={(text) => setAge(text)}
+        value={age}
+      />
+
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingHorizontal: 16,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 16,
+    height: 40,
   },
 });
+
+export default LoginScreen;
