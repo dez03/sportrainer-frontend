4e0007f5f8071e2704bd53531c572f8f8657bfb6
Aviel <hernandezaviel@gmail.com>
2023-09-25 23:04:03 -0700

    for school

--- App.js
+++ App.js
@@ -1,56 +1,85 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [age, setAge] = useState("");
+  const [errorMessage, setErrorMessage] = useState();
+  const handleLogin = async() => {
+    const response = await fetch(
+      `https://4a7vtq0f3e.execute-api.us-east-1.amazonaws.com/dev/user/signup`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          age: age,
+          firstName: firstName,
+        }),
+      }
+    );
+    const data = await response.json();
+    console.log(data)
 
-  const handleLogin = () => {
-   
+    if (data.success){
+      // TODO go to different page once user sucessfully registers
+    }
+    setErrorMessage(data.message)
+    return;
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Email:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        onChangeText={(text) => setEmail(text)}
-        value={email}
-        autoCapitalize="none" // Disable capitalization
-        keyboardType="email-address" // Set keyboardType to 'email-address'
-      />
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text style={styles.label}>Email:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          autoCapitalize="none" // Disable capitalization
+          keyboardType="email-address" // Set keyboardType to 'email-address'
+        />
 
-      <Text style={styles.label}>Password:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry={true}
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-      />
+        <Text style={styles.label}>Password:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+        />
 
-      <Text style={styles.label}>First Name:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="First Name"
-        onChangeText={(text) => setFirstName(text)}
-        value={firstName}
-      />
+        <Text style={styles.label}>First Name:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="First Name"
+          onChangeText={(text) => setFirstName(text)}
+          value={firstName}
+        />
+        {/* // TODO Make age a dropdown */}
+        <Text style={styles.label}>Age:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Age"
+          keyboardType="numeric"
+          onChangeText={(text) => setAge(text)}
+          value={age}
+        />
+        {/* <Link href="/app/home"> */}
+        <Button title="Sign up" onPress={handleLogin} />
+        {/* </Link> */}
 
-      <Text style={styles.label}>Age:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Age"
-        keyboardType="numeric"
-        onChangeText={(text) => setAge(text)}
-        value={age}
-      />
-
-      <Button title="Login" onPress={handleLogin} />
-    </View>
+        {/* //TODO add loading button and disable the button when the API is getting called, aka style this whole page */}
+        <Text>{errorMessage}</Text>
+      </View>
+    </NavigationContainer>
   );
 };
 
---
+++ home.js
@@ -0,0 +1,9 @@
+// import { View } from "react-native";
+
+// export default function Page() {
+//   return (
+//     <View>
+//       <h1> HOME</h1>
+//     </View>
+//   );
+// }
